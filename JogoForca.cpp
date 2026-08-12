a92d1c0c4e424cb8179ba259b987c7834ac8654c
Marco Thullyo <marcothullyo74@gmail.com>
2026-08-12 18:05:48 -0300

    Correcao e explicacoes do buffer

--- JogoForca.cpp
+++ JogoForca.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 
 
@@ -32,8 +33,32 @@ int main() {
         
         std::cout << std::endl;
         std::cout << "Minha escolha eh:\n";
-        std::cin >> escolha;
-        std::cout << std::endl;
+        std::cin >> escolha;    
+
+        //ao digitar 42, por exemplo, no buffer fica 42\n
+        //entao, ao passar pelo loop, ele entra na primeira opcao (if escolha != 1 && escolha != 2...)
+        //mas quando volta aqui, o \n esta sozinho. Entao o compilador passa direto pelo cin, achando que ja tem outra resposta
+
+
+        std::cin.clear();   //limpa flags de erro. ex: digitou string ao inves de int
+
+        //std::cin.ignore(100000, '\n'); ignore caracteres ate atingir o limite OU ate encontrar o delimitador
+        //ex: supondo que o parametro é (100, \n)
+        //usuario digita um numero de 50 caracteres. Ele ignora os 50 caracteres e encontra o \n. Funciona
+        //usuario digita um numero de 150 caracteres. Ele ignora os 100 primeiros e PARA mesmo sem ter encontrado o \n
+        
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        //nesse contexto, ignorar significa que ele vai percorrer o numero de elementos que se encontra no primeiro parametro
+        //ate chegar no segundo parametro. Entao em (100, \n) ele percorreria os 100 elementos, mas se encontrasse o  \n antes, pararia
+        //(Aqui, o sentido de ignorar os 100 primeiros elementos eh que ele esta na busca pelo \n)
+        //(agora, quando ele encontra o \n, ai ele ignora o \n. Se ele passa de 100 caracteres, da problema, porque o \n esta fora desse alcance de 100 caracteres)
+   
+        //O PULO DO GATO
+        //O ignore NAO DESCARTA O 42, por exemplo. O numero que voce digitou ja foi consumido e esta na variavel "escolha"
+        //O ignore SO VE O QUE SOBROU NO BUFFER
+
+        std::cout << std::endl; 
 
         if (escolha != 1 && escolha != 2 && escolha != 3) {
                 std::cout << "Escolha errada! Mais atencao na proxima vez!\n";
