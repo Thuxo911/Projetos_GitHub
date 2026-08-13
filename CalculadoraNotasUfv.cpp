a1cd1a0e631b4323f496abfd147769a1b2def628
Marco Thullyo <marcothullyo74@gmail.com>
2026-08-13 07:32:28 -0300

    Estagio inicial

---
+++ CalculadoraNotasUfv.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+
+int main () {
+
+    bool checagem = true;
+
+
+    while (checagem) {
+
+        std::cout << "==========BEM-VINDO(A)==========\n";
+        std::cout << "======CALCULADORA DE NOTAS======\n";
+        std::cout << std::endl;
+        
+        std::cout << "Bem-vindo(a), professor da UFV. Voce esta no menu inicial\n";
+        std::cout << ""
+
+
+
+    }
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
